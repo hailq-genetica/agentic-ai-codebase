278996f2a9fc7a38c8eba09535d9bfe2e732e673
Hai Luong Quang <hai.luong@genefriendway.com>
2026-06-25 15:03:46 +0700

    Fix query_pdb_identifiers parameter issue

--- src/tool_registry/external_database.py
+++ src/tool_registry/external_database.py
@@ -169,8 +169,8 @@ description = [
         "required_parameters": [
             {
                 "name": 'identifiers',
-                "type": 'str',
-                "description": 'Parameter identifiers',
+                "type": 'list of str',
+                "description": 'List of PDB identifiers string. Pass a list even for a single identifier.',
                 "default": None
             },
         ],
@@ -189,8 +189,8 @@ description = [
             },
             {
                 "name": 'attributes',
-                "type": 'str',
-                "description": 'Parameter attributes',
+                "type": 'list of str',
+                "description": 'List of attributes to retrieve. Pass a list even for a single attribute.',
                 "default": None
             },
         ],
